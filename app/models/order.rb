9635a453c2e22c6e5d737b46c690cf8b2a463d24
davidkerr766 <david.kerr766@outlook.com>
2020-05-12 09:15:31 +1000

    total logic added to lineitem and order modelsand order view

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,12 @@ class Order < ApplicationRecord
 
   has_many :line_items, dependent: :destroy
   has_many :products, through: :line_items
+
+  def order_total
+    sum = 0
+    self.line_items.each { |item|
+      sum += item.total
+    }
+    return sum
+  end
 end
