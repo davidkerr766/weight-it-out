4dce93a07e9afea60bf76dc440b4cd479fb19a0f
davidkerr766 <david.kerr766@outlook.com>
2020-05-11 16:03:26 +1000

    lineitem logic working to add product to order

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root "products#index"
   resources :products
   devise_for :users
+
+  post 'line_items' => "line_items#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
